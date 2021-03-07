66a457267f07a38b2372ea16f2577fbcdb25c1ec
James King <ripixel@gmail.com>
2021-03-07 23:05:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Care for a walk?
+
+Simple progress bar to show how far a given number of kilometres is against some pre-set waypoints
---
+++ src/components/ProgressBar/styles.scss.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+  readonly "container": string;
+  readonly "label": string;
+  readonly "labelMarker": string;
+  readonly "labels": string;
+  readonly "progress-bar": string;
+  readonly "progressBar": string;
+  readonly "progressBarContainer": string;
+  readonly "stripes-move": string;
+};
+export = styles;
+
